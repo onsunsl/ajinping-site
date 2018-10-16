c8a3b9ecca6bccd35ac8dca8abb52ed7d024b3d1
onsunsl <onsunsl@foxmail.com>
2018-10-16 10:46:56 +0800

    添加工程框架代码

---
+++ ajinping/apps/__init__.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "onsunsl"
+# Date: 2018/10/16
+
---
+++ ajinping/apps/img_convert/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ImgConvertConfig(AppConfig):
+    name = 'img_convert'
